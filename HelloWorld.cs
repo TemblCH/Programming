fc9200eea50c81eef62769576f369ca51824dc76
TemblCH <157202333+TemblCH@users.noreply.github.com>
2024-01-20 11:11:35 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start вызывается перед обновлением первого кадра
+    void Start()
+    {
+        print("Hello World");
+        
+    }
+
+    // Обновление вызывается один раз для каждого кадра
+    void Update()
+    {
+        
+    }
+}
